be3f028c3e4b862ee78ed107a0e51bde21059c2c
jayval254 <valariekimuge1@gmail.com>
2023-03-22 08:46:21 -0700

    monty

--- monty.h
+++ monty.h
@@ -1,14 +1,14 @@
-#ifndef _MONTY_H
-#define _MONTY_H
+#ifndef MONTY_H
+#define MONTY_H
 
+
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <string.h>
-#include <errno.h>
+#include <ctype.h>
+
+extern char *number;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -40,21 +40,19 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* extern int nodeval; */
-extern int nodeval;
-
+void readfile(char *myfile);
+char **validateBuffer(char *buffer, int pointer, FILE *montyF);
+void op_val(char **command, char *buffer, stack_t **list, int line, FILE *a);
+void f_opcode(char **command, char *buffer, int line, stack_t **list, FILE *m);
+int _memory(char *buffer);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t **stack);
 void sub(stack_t **stack, unsigned int line_number);
-int parse(char *line, stack_t **stack, unsigned int line_number);
-int chkopcode(char *opcode, stack_t **stack, unsigned int line_number);
-void free_Stack(stack_t **stack);
 
 #endif
